26cfdf8bd2ee007a25017d30a8d1d01fb94b99df
Naveen Kumar M <naveen.kumar1@Naveens-MacBook-Pro.local>
2021-08-01 11:23:17 +0530

    Initial Commit

---
+++ git-practice/src/com/naveen/Main.java
@@ -0,0 +1,10 @@
+package com.naveen;
+
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hellow World");
+    }
+}
